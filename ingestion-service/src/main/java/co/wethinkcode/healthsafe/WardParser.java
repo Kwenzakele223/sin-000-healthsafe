c097033bed7552782a832877533a5c9db44b799f
Kwenzakele Shongwe <kwenzakeleshongwe53@gmail.com>
2026-09-23 15:27:49 +0200

    Setting up the TDDs (Green)

---
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/WardParser.java
@@ -0,0 +1,10 @@
+package co.wethinkcode.healthsafe;
+
+class WardParser{
+
+    public String normalizeWardId(String wardId)
+    {
+        String upper = wardId.toUpperCase();
+        return upper;
+    }
+}
